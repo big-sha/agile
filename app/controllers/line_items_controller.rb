17b67a7fc491088c7849d80154dc146456f29ad9
Sean Maurice Perkins <sean.perkins@rcn.net>
2014-02-06 17:25:28 -0500

    Smarter Cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -42,13 +42,12 @@ class LineItemsController < ApplicationController
   def create
     @cart = current_cart
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build
+    @line_item = @cart.add_product(product.id)
     @line_item.product = product
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart,
-          notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.json { render json: @line_item,
           status: :created, location: @line_item }
       else
@@ -56,7 +55,7 @@ class LineItemsController < ApplicationController
         format.json { render json: @line_item.errors,
           status: :unprocessable_entity }
       end
-    end
+   end 
   end
 
   # PUT /line_items/1
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,9 @@
 class LineItem < ActiveRecord::Base
- belongs_to :product
- belongs_to :cart
+  belongs_to :product
+  belongs_to :cart
+  attr_accessible :cart_id, :product_id
+
+  def total_price
+    product.price * quantity
+  end
 end
