0cdbb9edd133f41ac748efd485b51f21f2164124
Sean Maurice Perkins <sean.perkins@rcn.net>
2014-02-07 16:47:58 -0500

    Test Ajax

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,5 @@
 class LineItem < ActiveRecord::Base
+  attr_accessible :product, :product_id, :id, :updated_at, :cart_id, :created_at
   belongs_to :product
   belongs_to :cart
   attr_accessible :cart_id, :product_id
